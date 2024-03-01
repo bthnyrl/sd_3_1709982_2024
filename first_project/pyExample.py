e0c815acd04c41955f91b96342309157098b3b8e
BatuhanYorul_KGTU <batuhan.yorul@ogr.gidatarim.edu.tr>
2024-03-01 11:28:17 +0000

    pyExamples done

--- first_project/pyExample.py
+++ first_project/pyExample.py
@@ -1,9 +1,46 @@
-# first python programs
-print("ola mundo!")
-# atribuir um valor a x x=3
-# atribuir um valor a y
+# first example
+print("\nola mundo!")
 x = 3
 y = 5
 y = "John"
 print("x= ", x)
-print("y= ", y)
+print("y= ", y, "\n")
+
+# second example
+print(type(x))
+print(type(y), "\n")
+
+# third example
+x = 5
+y = "John"
+Y = "case-sensitve"
+print(x, y, Y, "\n")
+
+# fourth example
+x = "Hello"
+print(x)
+y = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(y, "\n")
+
+# fifth example
+x = "Python is "
+y = "awesome"
+z = x + y
+x = 5
+y = 10
+print(z, ",", x + y, "\n")
+
+# sixth example
+x = 5
+y = 2
+if x > y:
+    print("x é maior que y")
+else:
+    print("x não é maior que y")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
